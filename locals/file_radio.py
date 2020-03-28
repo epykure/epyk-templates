4836563e7c14fb94d7e8baba63a437e543482bff
marlyk <leemark0071@gmail.com>
2020-03-28 20:03:29 +0000

    radio

--- locals/file_radio.py
+++ locals/file_radio.py
@@ -1,10 +1,22 @@
 
 from epyk.core.Page import Report
+from epyk.tests import data_urls
+
 import config
 
 
 # Create a basic report object
 rptObj = Report()
 
+#
+data = rptObj.py.requests.csv(data_urls.AIRPORT_TRAFFIC, store_location=config.OUTPUT_TEMPS)
+
+#
+rptObj.body.style.css.padding = "0 20px"
+
+# Console component
+c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
+
+rptObj.ui.radio(data, column='city')
 
 print(rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name="report_radio"))
