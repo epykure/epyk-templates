919530935490917e3b73a0850d454ce05477511d
marlyk <leemark0071@gmail.com>
2020-03-28 21:18:27 +0000

    Update file_radio.py

--- locals/file_radio.py
+++ locals/file_radio.py
@@ -17,6 +17,18 @@ rptObj.body.style.css.padding = "0 20px"
 # Console component
 c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
 
+#
 rptObj.ui.radio(data, column='city')
+
+#
+rptObj.ui.fields.checkbox(True, label="Check")
+rptObj.ui.fields.radio(True, label="Check")
+
+#
+rptObj.ui.inputs.checkbox(True, label="Check")
+rptObj.ui.inputs.radio(True, label="Check")
+
+#
+rptObj.ui.icons.tick("Check")
 
 print(rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name="report_radio"))
