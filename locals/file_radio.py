2e719000d60c20ea689d941a548f0f71bac80804
marlyk <leemark0071@gmail.com>
2020-03-29 17:38:23 +0100

    radios

--- locals/file_radio.py
+++ locals/file_radio.py
@@ -18,17 +18,37 @@ rptObj.body.style.css.padding = "0 20px"
 c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
 
 #
-rptObj.ui.radio(data, column='city')
+# rptObj.ui.radio(data, column='city')
 
 #
-rptObj.ui.fields.checkbox(True, label="Check")
-rptObj.ui.fields.radio(True, label="Check")
+c = rptObj.ui.fields.checkbox(True, label="Check")
+c.click([
+  rptObj.js.console.log(c.dom.content)
+])
+
+r = rptObj.ui.fields.radio(True, label="Check 1", group_name="group1")
+r1 = rptObj.ui.fields.radio(True, label="Check 2", group_name="group1")
+
+r.click([
+  rptObj.js.console.log(r.dom.content)
+])
 
 #
 rptObj.ui.inputs.checkbox(True, label="Check")
 rptObj.ui.inputs.radio(True, label="Check")
 
 #
-rptObj.ui.icons.tick("Check")
+rptObj.ui.icons.tick(False, "Check")
+
+
+rptObj.ui.lists.radios([
+  {"value": True, 'label': 'Python'},
+  {"value": False, 'label': 'Javascript'},
+])
+
+
+data = [{"label": "python", "value": False}, {"label": "Java", "value": 5}]
+checks = rptObj.ui.lists.checklist(data)
+
 
 print(rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name="report_radio"))
