74784890d6ec405026f81ec2920e086105da398b
marlyk <leemark0071@gmail.com>
2020-03-31 18:45:11 +0100

    Update file_jquery_ui_progressbar.py

--- locals/file_jquery_ui_progressbar.py
+++ locals/file_jquery_ui_progressbar.py
@@ -1,10 +1,26 @@
 
 from epyk.core.Page import Report
-from epyk.tests import data_urls
 
 import config
 
 rptObj = Report()
 
+# Console component
+c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
+
+# Create a progressbar (max 100)
+b1 = rptObj.ui.sliders.progressbar(300, total=300)
+b1.options.background = "red"
+
+b2 = rptObj.ui.sliders.progressbar(50)
+
+# Add event on the progress bar to change and retrieve the value
+rptObj.ui.button("click").click([
+  c.write(b2.dom.content),
+  b1.build(20),
+  c.write(b1.dom.val, stringify=True),
+])
+
+c.move()
 
 print(rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name="report_jquery_ui_progressbar"))
