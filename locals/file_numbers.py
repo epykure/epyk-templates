9060a08451f3b1f863c6df2552f428e6f5a15f18
marlyk <leemark0071@gmail.com>
2020-04-02 21:41:46 +0100

    Update file_numbers.py

--- locals/file_numbers.py
+++ locals/file_numbers.py
@@ -9,8 +9,25 @@ rptObj = Report()
 c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
 
 #
-rptObj.ui.button("click").click([
+n = rptObj.ui.numbers.digits(76862.095)
+n.style.color = "red"
 
+
+n1 = rptObj.ui.text(76862.095)
+
+n1.onReady([
+  n1.dom.format.number.toFixed(6)
+])
+
+rptObj.ui.button("display").click([
+  rptObj.js.alert(n1.dom.content.number.toFixed(5)),
+  c.write(n1.dom.val, stringify=True)
+])
+
+#
+rptObj.ui.button("click").click([
+  n1.build(-38866),
+  n1.dom.format.toMoney('£')
 ])
 
 c.move()
