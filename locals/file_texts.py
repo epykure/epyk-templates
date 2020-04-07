6fb915760718c91dc790398dabd76f2e2fd30080
marlyk <leemark0071@gmail.com>
2020-04-07 22:45:15 +0100

    Update file_texts.py

--- locals/file_texts.py
+++ locals/file_texts.py
@@ -6,6 +6,9 @@ import config
 # Create a basic report object
 rptObj = Report()
 
+# Console component
+c = rptObj.ui.rich.console("* This is a log section for all the events in the different buttons *", options={"timestamp": True})
+
 rptObj.ui.text("this is a test")
 rptObj.ui.texts.label("this is a test", color="red").css({"float": 'none'})
 rptObj.ui.texts.span("Test").css({"border": "1px solid black"})
@@ -22,7 +25,8 @@ rptObj.ui.images.badge("This is a badge", background_color="red", color="white")
 rptObj.ui.layouts.new_line()
 rptObj.ui.images.icon("fab fa-angellist")
 
-rptObj.ui.texts.blockquote("This is a blockquote")
+quotation = rptObj.ui.texts.blockquote("If you decide to design your own language, there are thousands of sort of amateur language designer pitfalls.", author="Guido van Rossum")
+quotation.style.css.padding = "0 50px"
 
 python = rptObj.ui.codes.python('''
 def test(a):
@@ -34,7 +38,8 @@ f = rptObj.ui.texts.fieldset("fieldset")
 f += rptObj.ui.title("test")
 f += rptObj.ui.texts.label("label")
 
-rptObj.ui.texts.preformat("This is a pre formatted text")
+pre = rptObj.ui.texts.preformat("This is a pre formatted text")
+
 
 rptObj.ui.texts.highlights("Test content", title="Test")
 
@@ -45,9 +50,15 @@ n = rptObj.ui.numbers.euro(4647666876)
 rptObj.ui.button("Test").click([
   f.build("Ok", {"css": {"color": 'red'}}),
   f[0].build("new title"),
+  c.write(pre.dom.content),
   n.build(34.5656),
-  python.refresh()
+  python.build('''
+def function(test):
+  print("RRRRRRRRRRRR")
+''')
 ])
+
+c.move()
 
 #rptObj.ui.texts.blockquote("This is a code")
 #rptObj.ui.texts.preformat("This is a pre formatted text")
