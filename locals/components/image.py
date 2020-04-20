7a3f789a8147841a90bb9d340ad8e70e9aefe953
marlyk <leemark0071@gmail.com>
2020-04-20 19:15:47 +0100

    Update image.py

--- locals/components/image.py
+++ locals/components/image.py
@@ -7,58 +7,54 @@ from epyk.core.css.themes import ThemeDark
 # Create a basic report object
 rptObj = Report()
 
-#
+# Change the theme of the report
 rptObj.theme = ThemeDark.Dark()
+
+# Change the first color of this theme (overrride)
 rptObj.theme.colors[-1] = "yellow"
 
-
+# Add icon
+# The path for the icon is defind in config
 rptObj.ui.images.circular("epykIcon.PNG", path=config.IMG_PATH)
 
 rptObj.ui.layouts.new_line()
+
+# Avatar with a status
 rptObj.ui.images.avatar("Epyk", status='out')
+
+# Avatar with an image
 rptObj.ui.images.avatar(image="epykIcon.PNG", path=config.IMG_PATH, status=False)
 
+# Add badges
 rptObj.ui.layouts.new_line()
 rptObj.ui.images.badge("Test badge", "Label", icon="fas fa-align-center")
+
+rptObj.ui.layouts.new_line()
 rptObj.ui.images.badge("This is a badge", background_color="red", color="white")
+
+rptObj.ui.layouts.new_line()
 rptObj.ui.images.badge(12, icon="far fa-bell", options={"badge_position": 'left'})
 
-
-car = rptObj.ui.images.carrousel(["epykIcon.PNG", "epyklogo.ico", "epyklogo_whole_big.png"],
-                                 path=config.IMG_PATH, height=(200, 'px'))
+# Add a carrousel of images
+car = rptObj.ui.images.carrousel(["epykIcon.PNG", "epyklogo.ico", "epyklogo_whole_big.png"], path=config.IMG_PATH, height=(200, 'px'))
 car.click([rptObj.js.console.log('data', skip_data_convert=True)])
 
-#
+# Add a simple badge
 c1 = rptObj.ui.images.badge(3, icon="far fa-bell", url="test")
 c1.style.display = None
+
+# Add the badge to a container on the right
 rptObj.ui.layouts.div([c1], align='right')
 rptObj.ui.button("display").click(c1.dom.toggle())
 
-# from epyk.core.html import Defaults
-#
-# Defaults.SERVER_PATH = r"C:\Pictures"
-# rptObj.ui.images.animated("philo.PNG")
-
+# Add an emoji
 rptObj.ui.images.emoji(rptObj.symbols.smileys.DISAPPOINTED_FACE)
-#
-# rptObj.ui.button("test")
-#
-# a = rptObj.ui.images.animated("epykIcon.PNG", text="This is a comment", title="Title", url="#", path=r"../../../static/images")
-# a.style.css.borders()
-#
-# b = rptObj.ui.images.animated("epykIcon.PNG", text="This is a comment", title="Title", url="#", path=r"../../../static/images")
-# b.style.css.borders()
-#
-# c = rptObj.ui.images.animated("epykIcon.PNG", text="This is a comment", title="Title", url="#", path=r"../../../static/images")
-# c.style.css.borders()
-#
-# rptObj.ui.layouts.row([a, b, c])
-#
-# rptObj.ui.button("test").click([
-#   c.title.build("ok"),
-#   c.text.build("this is a test")
-# ])
 
+# Add an animated picture
+a = rptObj.ui.images.animated("epykIcon.PNG", text="This is a comment", title="Title", url="#", path=config.IMG_PATH)
+a.style.css.borders()
+
+# Add multiple badge on the same row
 rptObj.ui.images.badge("306", url="google", icon="fas fa-align-center", options={'badge_css': {'color': 'white', "background": 'red'}})
 rptObj.ui.images.badge("306", url="google", icon="fas fa-align-center", options={'badge_css': {'color': 'white', "background": 'orange'}})
 rptObj.ui.images.badge("306", url="google", icon="fas fa-align-center", options={'badge_css': {'color': 'white', "background": 'red'}})
@@ -73,8 +69,7 @@ t = rptObj.ui.text("Test")
 t.style.font_size = 20
 t.style.effects.translate(1)
 
-
+# Add the epyk icon
 rptObj.ui.icons.epyk()
 
-#rptObj.style.imports("toto")
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
