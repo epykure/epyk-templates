1bda42136a497f926cbfa889fd966dc519fbe3f7
marlyk <leemark0071@gmail.com>
2020-05-02 18:21:48 +0100

    Create postit.py

---
+++ locals/components/postit.py
@@ -0,0 +1,22 @@
+
+
+from epyk.core.Page import Report
+from epyk.tests import data_urls
+
+import config
+rptObj = Report()
+
+rptObj.ui.layouts.new_line(10)
+
+example = rptObj.ui.title("Example")
+text = rptObj.ui.text("This is a text")
+example.style.css.color = 'red'
+data_rest = rptObj.py.requests.csv(data_urls.PLOTLY_APPLE_PRICES, store_location=config.OUTPUT_TEMPS)
+ts = rptObj.ui.charts.chartJs.timeseries(data_rest, y_columns=['AAPL.Open'], x_axis="Date")
+
+p = rptObj.ui.postit([example, text, ts])
+p.anchor.style.css.margin_left = '50px'
+p.popup.style.css.height = "250px"
+p.popup.style.css.width = "300px"
+
+rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
