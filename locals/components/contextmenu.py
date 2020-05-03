090b29877997847d1d5982652b48100433b995ae
marlyk <leemark0071@gmail.com>
2020-05-03 12:56:19 +0100

    Update contextmenu.py

--- locals/components/contextmenu.py
+++ locals/components/contextmenu.py
@@ -7,7 +7,8 @@ rptObj = Report()
 rptObj.headers._favicon_url = config.FAVICON_URL # Change the Epyk logo
 
 #
-menu = rptObj.ui.context_menu(["Super", 'next'])
+menu = rptObj.ui.contextual(["Super", 'next'])
+menu.add_item('test', 'fas fa-times')
 menu[0].click([
   rptObj.js.alert("Test")
 ])
@@ -16,7 +17,7 @@ rptObj.ui.layouts.new_line(10)
 t = rptObj.ui.text("Test")
 t.contextMenu(menu, [])
 
-menu2 = rptObj.ui.context_menu([])
+menu2 = rptObj.ui.contextual([])
 for d in [{'label': 'Python', 'url': 'https://www.python.org/'}, {'label': 'R'}]:
   menu2 += rptObj.ui.link(d['label'], d.get('url'))
 
@@ -24,5 +25,20 @@ rptObj.ui.layouts.new_line(5)
 t2 = rptObj.ui.text("Test")
 t2.contextMenu(menu2, [])
 
+i = rptObj.ui.fields.input("test", label="test", options={"select": True})
+i2 = rptObj.ui.fields.integer(options={"quantity": True, "select": True})
+rptObj.ui.fields.static(label="readonly field")
+
+b = rptObj.ui.button("Click")
+rptObj.ui.navigation.side([i, i2, b], position="left")
+# size = 200
+# d = rptObj.ui.div("XXXXXXXXXXXXXXX")
+# d.css({"position": 'absolute', 'top': 0, 'right': 0, 'height': '100%', 'overflow-x': 'hidden', 'margin-right': "-%spx" % size,
+#        'width': "%spx" % size, 'border-left': '1px solid %s' % rptObj.theme.colors[5], 'padding': '5px'})
+# rptObj.body.style.css.overflow_x = 'hidden'
+#
+# rptObj.ui.button("Click").click([
+#   d.dom.toggle_transition("margin-right", "0px", "-%spx" % size),
+# ])
 
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
