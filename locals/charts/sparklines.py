e3fa60260ea3ced19788f15e71447c520690e978
marlyk <leemark0071@gmail.com>
2020-05-05 21:58:59 +0100

    Sparkline

--- locals/charts/sparklines.py
+++ locals/charts/sparklines.py
@@ -6,8 +6,17 @@ import config
 rptObj = Report()
 
 rptObj.ui.charts.sparkline("box", [1, 2, 3, 4, 5, 4, 3, 2, 1])
-rptObj.ui.charts.sparkline("bar", [1, 2, 3, 4, 5, 4, 3, 2, 10])
+l = rptObj.ui.charts.sparkline("line", [1, 2, 3, 4, 5, 4, 3, 2, 10])
+l.click([
+  rptObj.js.console.log(l.dom.val),
+  rptObj.js.console.log(l.dom.content),
+  rptObj.js.console.log(l.dom.offset),
+])
 
-rptObj.ui.charts.sparkline("bar", [1, 2, 3], title="Top Clients (#)",  options={"barWidth": 20})
-
+s = rptObj.ui.charts.sparkline("bar", [1, 2, 3], title="Top Clients (#)",  options={"barWidth": 20})
+s.hover([
+  rptObj.js.console.log(s.dom.val),
+  rptObj.js.console.log(s.dom.content),
+  rptObj.js.console.log(s.dom.offset),
+])
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
