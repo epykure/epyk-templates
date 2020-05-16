08d244ce6fd78e4089cee379222ab7ad8511ef43
marlyk <leemark0071@gmail.com>
2020-05-16 12:51:25 +0100

    geo charts

---
+++ locals/geo/geo_plotly.py
@@ -0,0 +1,39 @@
+
+from epyk.core.Page import Report
+from epyk.tests import data_urls
+
+import config
+
+rptObj = Report()
+rptObj.headers._favicon_url = config.FAVICON_URL # Change the Epyk logo
+
+
+records = [
+  {'countries': 'FRA', 'size': 20},
+  {'countries': 'GBR', 'size': 50},
+  {'countries': 'DEU', 'size': 30},
+  {'countries': 'RUS', 'size': 15},
+  {'countries': 'ESP', 'size': 15},
+]
+
+
+data_countries = rptObj.py.requests.csv(data_urls.PLOTLY_COUNTRIES, store_location=config.OUTPUT_TEMPS)
+
+europe = rptObj.ui.geo.plotly.choropleths.europe(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+asia = rptObj.ui.geo.plotly.choropleths.asia(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+africa = rptObj.ui.geo.plotly.choropleths.africa(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+sa = rptObj.ui.geo.plotly.choropleths.south_america(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+na = rptObj.ui.geo.plotly.choropleths.north_america(data_countries, size_col='GDP (BILLIONS)', country_col='CODE', height=(600, 'px'))
+
+rptObj.ui.button("Click").click([
+  europe.build(rptObj.data.plotly.choropleth(records, 'countries', 'size'))
+])
+
+rptObj.ui.grid([
+  [na],
+  [europe, asia],
+  [africa, sa],
+])
+
+
+rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
---
+++ locals/geo/geo_plotly_bubble.py
@@ -0,0 +1,30 @@
+
+from epyk.core.Page import Report
+from epyk.tests import data_urls
+
+import config
+
+rptObj = Report()
+rptObj.headers._favicon_url = config.FAVICON_URL # Change the Epyk logo
+
+
+data_countries = rptObj.py.requests.csv(data_urls.PLOTLY_COUNTRIES, store_location=config.OUTPUT_TEMPS)
+
+data_geo = rptObj.py.requests.csv(data_urls.GEO_US_CITIES, store_location=config.OUTPUT_TEMPS)
+
+m = rptObj.ui.geo.plotly.bubbles.usa(data_geo, size_col='pop', long_col='lon', lat_col='lat', options={"scale": 50000, 'size': 1})
+
+sa = rptObj.ui.geo.plotly.bubbles.south_america(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+w = rptObj.ui.geo.plotly.bubbles.world(data_countries, size_col='GDP (BILLIONS)', country_col='CODE')
+
+rptObj.ui.row([w, sa])
+
+records = [
+  {'location': 'GBR', 'value': 40}
+]
+rptObj.ui.button("Click").click([
+  #w.build(rptObj.data.plotly.locations(data_geo, 'location', 'value'))
+  w.build(rptObj.data.plotly.locations(data_geo, 'lon', 'lat', 'pop'))
+])
+
+rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
