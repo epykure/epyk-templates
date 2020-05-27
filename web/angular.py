1b51b304ce33fb9108f39002513c8a6f0bfb9da0
marlyk <leemark0071@gmail.com>
2020-05-27 22:18:48 +0100

    template

--- web/angular.py
+++ web/angular.py
@@ -4,10 +4,26 @@ sys.path.append(r"../../epyk-ui")
 
 from epyk.web import angular
 
-#npms = angular.requirements(rptObj)
-app = angular.app(r"C:\Angular\test-app")#
-app.npm(["chartjs-plugin-zoom"])
-#app.ng("test-app").lint() #.generate('component', 'test')
+app = angular.Angular(app_path=r"C:\Angular", name="my-first-app")
 
-#app.npm(npms)
-#app.install()
+page = app.page(None, "test2", 'AppComponentTest2')
+
+chartBarId = page.components.chart({'values': [10, 10, 60, 5], 'type': 'bar', 'labels': ["A", "B", "C", "D"]})
+chartPieId = page.components.chart({'values': [10, 10, 60, 5], 'type': 'pie', 'labels': ["A", "B", "C", "D"]})
+table = page.components.table({'values': [10, 10, 60, 5], 'type': 'pie', 'labels': ["A", "B", "C", "D"]})
+print(chartBarId, chartPieId)
+
+page.components.input()
+page.components.button("Test", 'update', [page.http("getData", ["console.log(this.input.nativeElement.value); this.%s.update(data); this.%s.update(data)" % (chartBarId, chartPieId) ])])
+
+#page.components.chart({'values': [10, 5, 2, 5], 'type': 'line', 'labels': ["A", "B", "C", "D"]})
+#page.components.chart({'values': [10, 10, 60, 5], 'type': 'scatter', 'labels': ["A", "B", "C", "D"]})
+
+app.publish()
+
+#route = app.route
+#route.add(component="AppTest", alias="test", path="./pr/app.component_test_new")
+
+#app.ng_modules().add("HttpClientModule", '@angular/common/http')
+#print( app.ng_modules() )
+#route.export()
