d3b9fa96940763b6fff88aacf37fff21fbaed516
marlyk <smith.pyotr@gmail.com>
2020-07-12 01:55:43 +0100

    Update home_tableau.py

--- websites/home_tableau.py
+++ websites/home_tableau.py
@@ -6,7 +6,15 @@ from epyk.core.Page import Report
 page = Report()
 page.headers.dev("https://www.tableau.com/favicon.ico")
 
-page.ui.navbar(logo="https://cdns.tblsft.com/sites/all/themes/tabwow/logo.png")
+logo = page.ui.div(height=(30, "px"), width=(30, "px"))
+logo.style.css.background_url("https://cdns.tblsft.com/sites/all/themes/tabwow/logo.png", size="auto 30px")
+logo.style.css.display = "inline-block"
+
+page.ui.navbar(logo=page.ui.div([logo, "Tableau"]), height=(80, "px"))
+
+bg = page.ui.images.background("https://cdns.tblsft.com/sites/default/files/hero/1366x455_desktop_hero_4.jpg", height=(400, "px"))
+bg.add(page.ui.button("Try tableau for free"))
+
 
 container = page.ui.div()
 title = page.ui.titles.title("Smart dashboards", width=("auto", ""))
@@ -21,6 +29,3 @@ col = page.ui.col([
 col.style.css.border_left = "1px solid black"
 container.add(page.ui.icons.awesome("fas fa-tv", width=(30, "px"), height=(30, "px")))
 container.add(col)
-
-bg = page.ui.images.background("https://cdns.tblsft.com/sites/default/files/hero/1366x455_desktop_hero_4.jpg")
-bg.add(page.ui.button("test"))
