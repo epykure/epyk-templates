1bc844b03c0989cad2aab880bf0d875cb4299652
marlyk <smith.pyotr@gmail.com>
2020-07-23 22:48:19 +0100

    allocine

---
+++ websites/material/allocine_data.py
@@ -0,0 +1,22 @@
+
+SITE = "http://www.allocine.fr/"
+
+FAVICON = "%s/favicon.ico" % SITE
+LOGO = "https://assets.allocine.fr/skin/img/allocine/logo-main-e4c626a7f3.svg"
+
+NEW_CINE = "DERNIÈRES NEWS CINÉMA"
+
+CARROUSEL = [
+  'http://fr.web.img4.acsta.net/carousels/20/07/20/17/02/1300735.jpg'
+]
+VIGNETS = [
+  {'title': "NEWS - Box Office",
+  'content': "Box-office France : 1 million de spectateurs en salles la semaine dernière",
+  'image': 'http://fr.web.img5.acsta.net/c_400_225/newsv7/20/07/23/10/33/3487734.jpg'},
+  {'title': "NEWS - Sorties Ciné",
+  'content': 'Radioactive : Marjane Satrapi "en colère" face au sexisme envers les réalisatrices',
+  'image': 'http://fr.web.img6.acsta.net/c_400_225/newsv7/20/07/23/11/26/4668763.jpg'},
+  {'title': "NEWS - Tournages",
+  'content': "Tetris : Taron Egerton (Rocketman) dans un film sur l'histoire du jeu vidéo",
+  'image': 'http://fr.web.img2.acsta.net/c_400_225/newsv7/20/07/23/09/52/0904713.jpg'},
+]
--- websites/templates/movies/allocine.py
+++ websites/templates/movies/allocine.py
@@ -2,6 +2,21 @@
 
 
 from epyk.core.Page import Report#
+from websites.material import allocine_data
 
 
 page = Report()
+page.headers.favicon(allocine_data.FAVICON)
+
+navbar = page.ui.navbar(logo=allocine_data.LOGO, height=(40, "px"), options={'logo_height': (20, 'px')})
+navbar[0].style.css.width = "200px"
+navbar.style.css.background_color = '#fecc00'
+
+page.ui.images.background(allocine_data.CARROUSEL[0], height=(350, "px"), size="contain")
+
+page.ui.title(page.py.encode_html(allocine_data.NEW_CINE)).css({"margin": '20px 1% 0 1%'})
+
+vignets = []
+for v in allocine_data.VIGNETS:
+  vignets.append(page.ui.vignets.image(title=page.py.encode_html(v['title']), image=v['image'], content=page.py.encode_html(v['content']), render="col"))
+row = page.ui.row(vignets)
