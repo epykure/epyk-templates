f6a2914930ca9a1449f4566d8acfcc228c13d2b8
marlyk <smith.pyotr@gmail.com>
2020-07-23 23:32:47 +0100

    allocine

--- websites/material/allocine_data.py
+++ websites/material/allocine_data.py
@@ -4,11 +4,20 @@ SITE = "http://www.allocine.fr/"
 FAVICON = "%s/favicon.ico" % SITE
 LOGO = "https://assets.allocine.fr/skin/img/allocine/logo-main-e4c626a7f3.svg"
 
+CINEMA = 'CINÉMA'
+CINE_GROUPS = [
+  "Avant-premières",
+  "Agenda des sorties",
+  "Films pour enfants à l'affiche",
+  "Meilleurs films",
+  "Box Office"
+]
 NEW_CINE = "DERNIÈRES NEWS CINÉMA"
 
 CARROUSEL = [
   'http://fr.web.img4.acsta.net/carousels/20/07/20/17/02/1300735.jpg'
 ]
+
 VIGNETS = [
   {'title': "NEWS - Box Office",
   'content': "Box-office France : 1 million de spectateurs en salles la semaine dernière",
--- websites/templates/movies/allocine.py
+++ websites/templates/movies/allocine.py
@@ -7,6 +7,8 @@ from websites.material import allocine_data
 
 page = Report()
 page.headers.favicon(allocine_data.FAVICON)
+page.body.style.css.padding = '0 60px'
+page.body.style.css.background = '#F2F2F2'
 
 navbar = page.ui.navbar(logo=allocine_data.LOGO, height=(40, "px"), options={'logo_height': (20, 'px')})
 navbar[0].style.css.width = "200px"
@@ -14,6 +16,19 @@ navbar.style.css.background_color = '#fecc00'
 
 page.ui.images.background(allocine_data.CARROUSEL[0], height=(350, "px"), size="contain")
 
+#
+title = page.ui.title(page.py.encode_html(allocine_data.CINEMA), align="center").css({"margin": '20px 1% 0 1%'})
+title.style.css.font_factor(25)
+
+row = []
+for grp in allocine_data.CINE_GROUPS:
+  row.append(page.ui.col([page.ui.div([
+    page.ui.div("#").css({"color": '#fecc00', 'width': 'auto', 'display': 'inline-block'}).style.css.font_factor(6),
+    page.ui.subtitle(page.py.encode_html(grp)).css({"white-space": 'nowrap','width': 'auto', 'display': 'inline-block'}),
+  ]).css({"white-space": 'nowrap'})], align="center"))
+page.ui.row(row)
+
+#
 page.ui.title(page.py.encode_html(allocine_data.NEW_CINE)).css({"margin": '20px 1% 0 1%'})
 
 vignets = []
