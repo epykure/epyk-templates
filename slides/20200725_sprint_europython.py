78eee5dff452d3bf997777c97e5dcaddcecb7981
Tinelsi972 <epykachu@gmail.com>
2020-07-25 03:24:11 +0100

    commit slides to presentation

--- slides/20200725_sprint_europython.py
+++ slides/20200725_sprint_europython.py
@@ -16,19 +16,72 @@ page.body.style.globals.size = 18
 slides = page.ui.vignets.slides(start=0)
 
 slides.style.css.display = "block"
-contents = page.ui.contents("Contente")
 
-slides.add_slide("Epyk - A web Studio library", [
-  page.ui.title('''
-A centralise library for linking   
-'''),
-  page.ui.img("epyklogo_whole_big.png?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images/", width=(300, 'px'))
+slides.add_slide("Epyk - A Web Studio library", [
+  page.ui.title('''A centralise library to control your whole stack''', level=2),
+  page.ui.layouts.new_line(2),
+  page.ui.img("epyklogo_whole_big.png?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images/", width=(300, 'px')),
 ])
 
 slides.add_slide("Epyk - Architecture", [
 
-  page.ui.img("architecture.PNG?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images", width=(500, 'px'))
+    page.ui.img("architecture.PNG?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images", width=(500, 'px'))
 ])
+
+
+slides.add_slide('Epyk - Simple rules', [
+                 page.ui.title('Easy to write code'),
+                 page.ui.texts.paragraph('''The philosophy behind the library structure is to allow both novice and advance developers to be able to start
+                 implementing their full stack solution really easily. In that effect novice developers don't have to write a lot of code before being able to
+                 implementing their solutions.
+                 On the other side more senior developers can deep dive into the library and start leveraging on the capabilities of javascript and all the other tools
+                 integrated in this framework''', width='500px'),
+                 page.ui.title('No dogma'),
+                 page.ui.texts.paragraph('''In order to let everyone find whatever they're looking for when using a "transpiling" library, we tried to make the
+                 library compatible with the most framework possible. Thanks to this, people coming from a front end development background will be able to 
+                 easily find their marks. We're also very interested in their feedback in order to improve the solution as a whole''', width='500px'),
+                ],)
+
+
+slides.add_slide("Epyk Studio - Ambition", [
+    page.ui.row([
+        page.ui.vignets.vignet(title='Core Library', content='''Improve and leverage on existing web framework to offer a maximum of features to developers'''),
+        page.ui.vignets.vignet(title='Mobile Expansion', content='''Start looking for ways to produce native apps from Python using the same philosophy'''),
+        page.ui.vignets.vignet(title='Knowledge Sharing', content='''In addition to the documentation, we have put in a place a huge number of notebooks for people to use as a knowledge base''')
+    ], align="center", position="top")
+])
+
+row = []
+for k, v in blablacar_data.CATEGORIES.items():
+    row.append(page.ui.vignets.vignet(title=k, content=v))
+
+button_style = {"border-radius": '20px', 'background-color': 'rgb(0, 175, 245)', 'color': 'white', 'padding': '8px 25px', 'font-size': '18px'}
+button_inv_style = {"border-radius": '20px', 'background-color': 'white', 'color': 'rgb(0, 175, 245)', 'padding': '8px 25px', 'font-size': '18px'}
+title_style = {"width": '90%', 'margin': 'auto', 'font-weight': 800, 'line-height': '1.06', 'font-size': '30px', 'color': 'rgb(5, 71, 82)', 'padding': '24px 0px'}
+
+button = page.ui.button(blablacar_data.BUTTON_OFFER_RIDW, align="center")
+button.style.css.margin_top = 10
+button.css(button_style)
+
+content = page.ui.col([page.ui.text(blablacar_data.SLOGAN, width=(100, "%")), button])
+content.style.css.padding = 0
+
+slides.add_slide('Epyk - Code Sample - 1', [
+                page.ui.title('Only a few lines of code allow to generate a complete working page', level=2),
+                page.ui.layouts.new_line(),
+                page.ui.img("macdonalds.PNG?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images/", width=(600, 'px')),
+                page.ui.layouts.new_line(2),
+                page.ui.img("macdonalds_results.PNG?raw=true", path="https://github.com/epykure/epyk-ui/blob/master/epyk/static/images/", width=(600, 'px')),]
+
+                 )
+
+slides.add_slide("Epyk - Code Sample - 2", [
+    page.ui.title("Car Company").css({"margin-top": '130px'}),
+    page.ui.vignets.image(blablacar_data.VIGNET_2['title'], content=content, image=blablacar_data.VIGNET_2['image']),
+    page.ui.titles.title(blablacar_data.TITLE_1).css(title_style),
+    page.ui.row(row, align="center", position="top")
+])
+
 
 # Using standard Javascript Libraries
 slides.add_slide("Epyk - Analytics", [
@@ -88,34 +141,24 @@ slides.add_slide("Epyk - Time Tracker (Agile Teams)", [
 ])
 
 
-# -------------------------
-# But also a real web studio
-
-row = []
-for k, v in blablacar_data.CATEGORIES.items():
-  row.append(page.ui.vignets.vignet(title=k, content=v))
-
-button_style = {"border-radius": '20px', 'background-color': 'rgb(0, 175, 245)', 'color': 'white', 'padding': '8px 25px', 'font-size': '18px'}
-button_inv_style = {"border-radius": '20px', 'background-color': 'white', 'color': 'rgb(0, 175, 245)', 'padding': '8px 25px', 'font-size': '18px'}
-title_style = {"width": '90%', 'margin': 'auto', 'font-weight': 800, 'line-height': '1.06', 'font-size': '30px', 'color': 'rgb(5, 71, 82)', 'padding': '24px 0px'}
-
-button = page.ui.button(blablacar_data.BUTTON_OFFER_RIDW, align="center")
-button.style.css.margin_top = 10
-button.css(button_style)
-
-content = page.ui.col([page.ui.text(blablacar_data.SLOGAN, width=(100, "%")), button])
-content.style.css.padding = 0
-
-
-slides.add_slide("Epyk - Web Studio", [
-  page.ui.title("Car Compagny").css({"margin-top": '130px'}),
-  page.ui.vignets.image(blablacar_data.VIGNET_2['title'], content=content, image=blablacar_data.VIGNET_2['image']),
-  page.ui.titles.title(blablacar_data.TITLE_1).css(title_style),
-  page.ui.row(row, align="center", position="top")
+slides.add_slide("Epyk - Where to find it ?", [
+        page.ui.title('Come take a look and maybe participate !', level=1),
+        page.ui.layouts.new_line(1),
+        page.ui.list([
+        page.ui.links.link('Pypi', 'https://pypi.org/project/epyk/', icon='fab fa-python'),
+        page.ui.links.link('Github', 'https://github.com/epykure/epyk-ui', icon='fab fa-github'),
+        page.ui.links.link('Documentation', 'http://www.epyk.io/', icon='fa fa-book'),
+        page.ui.links.link('Templates', 'https://github.com/epykure/epyk-templates', icon='fa fa-file'),
+        page.ui.links.link('epykachu@gmail.com', 'epykachu@gmail.com', icon='fa fa-envelope'),
+            ])
 ])
 
-page.body.onReady([
-  contents.build([
-    {"anchor": '#test', 'level': 1, 'text': 'Overview'}
-  ])
-])
+
+
+# page.body.onReady([
+#   contents.build([
+#     {"anchor": '#test', 'level': 1, 'text': 'Overview'}
+#   ])
+# ])
+
+print(page.outs.html_file(name='europython_slides'))
